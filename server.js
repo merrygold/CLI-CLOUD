71bb9656460127a85d7562fcd41c1fe85c24ed70
merrygold <merrygold643@gmail.com>
2023-09-18 21:24:22 +0500

    project

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
 const app = express();
-const port = 3000;
 
+const port = process.env.PORT || 3000;
 dotenv.config();
 app.use(cors());
 
